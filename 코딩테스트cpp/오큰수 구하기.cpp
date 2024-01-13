72ff78f74fb5f25fa81546e2192f59c73b4792e7
wonjun Lee <wj6715@naver.com>
2024-01-13 21:47:50 +0900

    오큰수 구하기 백준 17298번

---
+++ 코딩테스트cpp/오큰수 구하기.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int N;
+
+	cin >> N;
+
+	vector<int> A(N, 0);
+	vector<int> ans(N, 0);
+	stack<int> myStack;
+
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+
+	myStack.push(0);
+	for (int i = 1; i < N; i++) {
+		while (!myStack.empty() && A[myStack.top()] < A[i]) {
+			ans[myStack.top()] = A[i];
+			myStack.pop();
+		}
+		myStack.push(i);
+	}
+	while (!myStack.empty()) {
+		ans[myStack.top()] = -1;
+		myStack.pop();
+	}
+
+	for (int i = 0; i < N; i++) {
+		cout << ans[i] << " ";
+	}
+
+}
